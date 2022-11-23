fb4dd235fc83f3be9fa6e658499cdee8cf601444
Paulina Zygmuntowicz <114671842+PaulinaZy@users.noreply.github.com>
2022-11-23 17:14:57 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const input = document.querySelector("#userInput");
+const btn = document.querySelector("#btn");
+const form = document.querySelector("#form");
+
+// Add click listener to button
+form.addEventListener("submit", validateEmail);
+
+//Creating a box msg
+let msg = document.createElement("div");
+msg.style.position = "absolute";
+msg.style.bottom = "-0.5rem";
+msg.style.width = "100%";
+msg.style.textAlign = "center";
+
+//function to validate email
+function validateEmail(e) {
+    e.preventDefault();
+    msg.remove();
+    form.appendChild(msg);
+
+    //regex for email validation 
+    const regexEmail = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm;
+
+    //if email is correct
+    if (regexEmail.test(input.value)) {
+        msg.style.color = "#54E6AF";
+        msg.innerText = "Hurray! Your access has been granted"
+        return true;
+    } else {
+        msg.style.color = "#FB3E3E";
+        msg.innerText = "Oops! Please check your email"
+        return false;
+    }
+}
